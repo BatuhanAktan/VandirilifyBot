d56ec612b262d9e4828807cdb72fbd161567f57e
BatuhanAktan <43014701+BatuhanAktan@users.noreply.github.com>
2021-01-29 11:14:32 -0500

    Updated Algorithm

---
+++ Procfile
@@ -0,0 +1,1 @@
+worker: python Vandirilify.py
--- Vandirilify.py
+++ Vandirilify.py
@@ -2,8 +2,8 @@ import discord, time, pickle, requests
 from discord.ext import commands
 from discord.utils import get
 
-
-
+key = 'AIzaSyCVHLbDSo8SMYhAqko1jRRf9j0BMHn6okQ'
+token = 'ODA0MzQ4NDk4Nzc3ODY2MzAx.YBLB6g.LZ6OYCoc1TETiMvLsKAAEaQFGMI'
 
 
 
@@ -17,7 +17,7 @@ client  = commands.Bot(command_prefix = '.')
 async def on_ready():
 	while True:
 		oldNumVids = pickle.load(open('numVids.pickle', 'rb'))
-		numVidResponse = requests.get('https://youtube.googleapis.com/youtube/v3/channels?part=statistics&forUsername=Vandiril&key={KEY}').json()
+		numVidResponse = requests.get('https://youtube.googleapis.com/youtube/v3/channels?part=statistics&forUsername=Vandiril&key={}'.format(key)).json()
 		numVids = int(numVidResponse['items'][0]['statistics']['videoCount'])
 
 		print("Old Vid: ", oldNumVids, "Num Vids:", numVids)
@@ -30,10 +30,16 @@ async def on_ready():
 			pickleOut.close()
 
 			print("Getting Video Url")
-			vid = requests.get('https://www.googleapis.com/youtube/v3/search?key={}&channelId={}&part=snippet,id&order=date&maxResults=20'.format(KEY,'UCZ-oWkpMnHjTJpeOOlD80OA')).json()
+			vid = requests.get('https://www.googleapis.com/youtube/v3/search?key={}&channelId={}&part=snippet,id&order=date&maxResults=20'.format(key,'UCZ-oWkpMnHjTJpeOOlD80OA')).json()
 			url = 'https://www.youtube.com/watch?v={}&ab_channel=Vandiril'.format(vid['items'][0]['id']['videoId'])
 			print("Vid Url:", url)
 			await client.get_channel(622483122373787652).send("<@&641790384095232000>","New Vandiril vid: ", url)
+
+		elif oldNumVids > numVids:
+			print("Video has been deleted, adjusting data")
+			pickleOut = open("numVids.pickle", "wb")
+			pickle.dump(numVids, pickleOut)
+			pickleOut.close()
 
 		else: 
 			print("No New Vids")
@@ -42,5 +48,5 @@ async def on_ready():
 		time.sleep(600)
 
 
-client.run(TOKEN)
+client.run(token)
 
